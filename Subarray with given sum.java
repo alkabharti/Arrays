60d67bf455e243ba39af8ca14ce255ac1749c0cb
alka <alka16010@iiitd.ac.in>
2022-02-10 18:44:46 +0530

    Create Subarray with given sum.java

---
+++ Subarray with given sum.java
@@ -0,0 +1,41 @@
+Subarray with given sum :
+Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S.
+  
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
+  
+------------------------------------------------------------------------------------------------------------------------------------------
+  
+Solution:
+static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        int i,j,sum=0,f=0;
+        ArrayList <Integer> array=new ArrayList<Integer>();
+        start:
+        for(i=0;i<n;i++)
+        {
+            sum=arr[i];
+            for(j=i+1;j<=n;j++)
+            {
+                if(sum==s)
+                {
+                    array.add(i+1);
+                    array.add(j);
+                    f=1;
+                    break start;
+                }
+                if(sum>s || j==n)
+                    break;
+                sum+=arr[j];
+            }
+        }
+        if(f==0)
+            array.add(-1);
+        return array;
+    }
+
