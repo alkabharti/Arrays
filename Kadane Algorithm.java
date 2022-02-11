0dd0dd81184e0e2cc030b6bf4e2e8cee720ea723
alka <alka16010@iiitd.ac.in>
2022-02-11 09:03:47 +0530

    Update and rename Kadane's Algorithm.java to Kadane Algorithm.java

--- Kadane Algorithm.java
+++ Kadane Algorithm.java
@@ -1,4 +1,4 @@
-Kadane's Algorithm :
+Kadane Algorithm :
 Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
 
 Example 1:  
